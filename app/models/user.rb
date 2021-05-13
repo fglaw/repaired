903540eff226123b8cb61e1f40289630c59561ce
fabian <fabiludos@gmail.com>
2021-05-13 21:30:48 +0200

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_attached :photo
+  has_one_attached :photo
 end
