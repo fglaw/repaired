74c9e2cb3ffd88b6ce1bbe545cd38fe92e4175c8
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-05-14 21:20:09 +0200

    added booking_id to review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_190223) do
+ActiveRecord::Schema.define(version: 2021_05_14_191844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,17 @@ ActiveRecord::Schema.define(version: 2021_05_14_190223) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.string "location"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "repair_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["repair_id"], name: "index_bookings_on_repair_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "repairs", force: :cascade do |t|
     t.string "type"
     t.integer "price"
@@ -57,6 +68,8 @@ ActiveRecord::Schema.define(version: 2021_05_14_190223) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -78,4 +91,7 @@ ActiveRecord::Schema.define(version: 2021_05_14_190223) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "repairs"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "bookings"
 end
