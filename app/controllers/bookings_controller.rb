16d3283c0c463a6ee8cfb9d8e364d84bc4692475
Jonathan Parmley <jonathanparmley@gmail.com>
2021-05-15 17:53:24 +0200

    Added map set up

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,29 +1,40 @@
 class BookingsController < ApplicationController
-
   def show
     @booking = Booking.find(params[:id])
     authorize @booking
+    # for geocode
+    @customer_marker = {
+        lat: 52.5200,
+        lng: 13.4050
+    }
+
+    @mechanic_marker = {
+        lat: 52.4125,
+        lng: 12.5316
+    }
+    @markers = [@customer_marker, @mechanic_marker]
+
   end
 
-    def new
-        @booking = Booking.new
-        authorize @booking
-    end
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
 
-    def create
-        @booking = Booking.new(booking_params)
-        authorize @booking
-        if @booking.save!
-            # for now redirect to homepage
-            redirect_to root_path notice: 'Booking was successfully created'
-        else
-            render :new
-        end
+  def create
+    @booking = Booking.new(booking_params)
+    authorize @booking
+    if @booking.save!
+      # for now redirect to homepage
+      redirect_to root_path notice: 'Booking was successfully created'
+    else
+      render :new
     end
+  end
 
-    private
+  private
 
-    def booking_params
-        params.require(:booking).permit(:location, :repair_id, :user_id, photos: [])
-    end
+  def booking_params
+    params.require(:booking).permit(:location, :repair_id, :user_id, photos: [])
+  end
 end
