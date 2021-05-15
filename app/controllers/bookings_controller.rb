11f59ff1d00ea4257e8bc22fd6af86697a2ffc31
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-05-15 12:39:39 +0200

    Booking_add routes and created controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+
+    def new
+        @booking = Booking.new
+        authorize @booking
+    end
+
+    def create
+        @booking = Booking.new(booking_params)
+        authorize @booking
+        if @booking.save!
+            # for now redirect to homepage
+            redirect_to root_path notice: 'Booking was successfully created'
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:location, :repair_id, :user_id, :photos: [])
+    end
+end
