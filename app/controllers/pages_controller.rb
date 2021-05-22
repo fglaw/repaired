06fbe2a4cedfa68b50e6a18bc4e2dc60ff0b27bd
fabian glaw <fabiludos@gmail.com>
2021-05-22 14:04:34 +0200

    dashboard sceleton

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,26 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @mechanic_bookings = Booking.where(mechanic_id: current_user.id)
-    @user_bookings = Booking.where(user_id: current_user.id)
+    if current_user.user_mechanic
+      active_bookings = Booking.where("mechanic_id = ? AND status = ?", current_user.id, "accepted")
+      @active_booking = active_bookings.first
+      
+      if active_bookings.length >  1
+        active_bookings.shift
+        @queued_bookings = active_bookings
+      else
+        @queued_bookings = [] 
+      end
+    
+      
+      @pending_bookings = Booking.where("mechanic_id = ? AND status = ?", current_user.id, "pending")
+      @history_bookings = Booking.where("mechanic_id = ? AND status = ?", current_user.id, "completed")
+
+    else
+    @history_bookings = Booking.where("user_id = ? AND status = ?", current_user.id, "completed")
+    @active_bookings = Booking.where("user_id = ? AND status = ? OR status = ?", current_user.id, "pending", "accepted")
+    
+    end
   end
 
 end
