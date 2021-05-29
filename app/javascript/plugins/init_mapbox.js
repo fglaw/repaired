be09ed67811a86f759c6a33b8cab3f3eb0f21559
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-05-29 15:55:53 +0200

    fixed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,7 +2,7 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { pulsingMarker } from './pulsing';
 
-   
+
 // Zoom function on booking new page
 const zoomMapToMaker = (map, marker) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -39,20 +39,20 @@ const addMarkerToMap = (map) => {
     
     markers.forEach((marker) => {
       let object = {
+        // color to purple
         color: '#63488C'
     }
       console.log('object_default:', object);
 
-      // change icon marker for customer
+      // set marker for customer
       if (marker.who == 'customer') {
         console.log('who:', marker.who);
-        // change color to red
         console.log('object_default:', object);
         new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
       }
 
-      // change icon marker for mechanic
+      // set marker for mechanic
       else if (marker.who == 'mechanic') {
         console.log('who:', marker.who);
     
@@ -77,13 +77,12 @@ const addMarkerToMap = (map) => {
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  var bike = document.createElement('div');
-      bike.className = 'marker';
-        console.log('bike:', bike);
-        // console.log('marker:', marker);
-
-  const marker = new mapboxgl.Marker(bike)
-    .setLngLat([ 13.4050, 52.5200 ])
+  // var bike = document.createElement('div');
+  //     bike.className = 'marker';
+  //       console.log('bike:', bike);
+  
+  // const marker = new mapboxgl.Marker(bike)
+  //   .setLngLat([ 13.4050, 52.5200 ])
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -93,106 +92,107 @@ const initMapbox = () => {
       attributionControl: false
     });
 
-    // to draw a pulsing dot icon on the map.
-var pulsingDot = {
-  width: 200,
-  height: 200,
-  data: new Uint8Array(200 * 200 * 4),
-   
-  // When the layer is added to the map,
-  // get the rendering context for the map canvas.
-  onAdd: function () {
-  var canvas = document.createElement('canvas');
-  canvas.width = this.width;
-  canvas.height = this.height;
-  this.context = canvas.getContext('2d');
-  },
-   
-  // Call once before every frame where the icon will be used.
-  render: function () {
-  var duration = 1000;
-  var t = (performance.now() % duration) / duration;
-   
-  var radius = (200 / 2) * 0.3;
-  var outerRadius = (200 / 2) * 0.7 * t + radius;
-  var context = this.context;
-   
-  // Draw the outer circle.
-  context.clearRect(0, 0, this.width, this.height);
-  context.beginPath();
-  context.arc(
-  this.width / 2,
-  this.height / 2,
-  outerRadius,
-  0,
-  Math.PI * 2
-  );
-  context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-  context.fill();
-   
-  // Draw the inner circle.
-  context.beginPath();
-  context.arc(
-  this.width / 2,
-  this.height / 2,
-  radius,
-  0,
-  Math.PI * 2
-  );
-  context.fillStyle = 'rgba(255, 100, 100, 1)';
-  context.strokeStyle = 'white';
-  context.lineWidth = 2 + 4 * (1 - t);
-  context.fill();
-  context.stroke();
-   
-  // Update this image's data with data from the canvas.
-  this.data = context.getImageData(
-  0,
-  0,
-  this.width,
-  this.height
-  ).data;
-   
-  // Continuously repaint the map, resulting
-  // in the smooth animation of the dot.
-  map.triggerRepaint();
-   
-  // Return `true` to let the map know that the image was updated.
-  return true;
-  }
-  };
+    // to draw a pulsing dot icon for customer on the map.
+    var pulsingDot = {
+      width: 100,
+      height: 100,
+      data: new Uint8Array(100 * 100 * 4),
+      
+      // When the layer is added to the map,
+      // get the rendering context for the map canvas.
+      onAdd: function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+      },
+      
+      // Call once before every frame where the icon will be used.
+      render: function () {
+        var duration = 1000;
+        var t = (performance.now() % duration) / duration;
+        
+        var radius = (100 / 2) * 0.3;
+        var outerRadius = (100 / 2) * 0.7 * t + radius;
+        var context = this.context;
+        
+        // Draw the outer circle.
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(
+        this.width / 2,
+        this.height / 2,
+        outerRadius,
+        0,
+        Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+        context.fill();
+        
+        // Draw the inner circle.
+        context.beginPath();
+        context.arc(
+        this.width / 2,
+        this.height / 2,
+        radius,
+        0,
+        Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+        
+        // Update this image's data with data from the canvas.
+        this.data = context.getImageData(
+        0,
+        0,
+        this.width,
+        this.height
+        ).data;
+        
+        // Continuously repaint the map, resulting
+        // in the smooth animation of the dot.
+        map.triggerRepaint();
+        
+        // Return `true` to let the map know that the image was updated.
+        return true;
+      }
+    };
 
     map.on('load', function () {
       map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
       map.addSource('dot-point', {
-      'type': 'geojson',
-      'data': {
-      'type': 'FeatureCollection',
-      'features': [
-      {
-      'type': 'Feature',
-      'geometry': {
-      'type': 'Point',
-      'coordinates': [13.4050, 52.5200] // icon position [lng, lat]
-      // 'maxzoom': 10
-      }
-      }
-      ]
-      }
+        'type': 'geojson',
+        'data': {
+          'type': 'FeatureCollection',
+          'features': [
+            {
+                'type': 'Feature',
+                'geometry': {
+                  'type': 'Point',
+                  // for customer
+                  'coordinates': [13.4050, 52.5200] // icon position [lng, lat]
+                  // 'maxzoom': 10
+                }
+            }
+          ]
+        }
       });
       map.addLayer({
-      'id': 'layer-with-pulsing-dot',
-      'type': 'symbol',
-      'source': 'dot-point',
-      'layout': {
-      'icon-image': 'pulsing-dot'
-      }
+        'id': 'layer-with-pulsing-dot',
+        'type': 'symbol',
+        'source': 'dot-point',
+        'layout': {
+          'icon-image': 'pulsing-dot'
+        }
       });
-      });
+    });
       // const bounds = new mapboxgl.LngLatBounds();
       // bounds.extend([13.4050, 52.5200])
       // map.fitBounds(bounds, { padding: 0, maxZoom: 15, duration: 0 });
-    // pulsingMarker(map, marker);
+      // pulsingMarker(map, marker);
     addMarkerToMap(map);
   }
 };
